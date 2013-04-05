f358a3c54636d9b2d871be57b550297cd09844c9
ayanamist <ayanamist@gmail.com>
2013-04-05 19:48:43 +0800

    Refactor, simplify code.

--- Twitter Traditional RT Style.user.js
+++ Twitter Traditional RT Style.user.js
@@ -20,25 +20,24 @@
         })
     };
 
-    function dasherize(str) {
+    var dasherize = function (str) {
         return str.replace(/::/g, '/')
             .replace(/([A-Z]+)([A-Z][a-z])/g, '$1_$2')
             .replace(/([a-z\d])([A-Z])/g, '$1_$2')
             .replace(/_/g, '-')
             .toLowerCase()
-    }
+    };
 
     var jOn = function (node, eventName, selector, listener) {
         node.addEventListener(eventName, function (event) {
             var matched = true,
                 target = event.target,
                 callback;
+
+            var matchesSelector = target.webkitMatchesSelector || target.mozMatchesSelector ||
+                target.oMatchesSelector || target.matchesSelector;
+
             if (typeof listener !== "undefined") {
-                var matchesSelector = target.webkitMatchesSelector || target.mozMatchesSelector ||
-                    target.oMatchesSelector || target.matchesSelector;
-                if (typeof matchesSelector === "undefined") {
-                    throw DOMException;
-                }
                 matched = matchesSelector.call(target, selector);
                 callback = listener;
             }
@@ -64,41 +63,41 @@
         return node.style.cssText += ';' + css;
     };
 
-    var addCSS = function (css) {
-        if (typeof GM_addStyle != "undefined") {
-            GM_addStyle(css);
-        } else if (typeof PRO_addStyle != "undefined") {
-            PRO_addStyle(css);
-        } else if (typeof addStyle != "undefined") {
-            addStyle(css);
-        } else {
-            var heads = document.getElementsByTagName("head");
-            if (heads.length > 0) {
-                var node = document.createElement("style");
-                node.type = "text/css";
-                node.appendChild(document.createTextNode(css));
-                heads[0].appendChild(node);
-                node = null;
+    if (typeof addStyle != "undefined") {
+        var addStyle = function (css) {
+            if (typeof GM_addStyle != "undefined") {
+                GM_addStyle(css);
+            } else if (typeof PRO_addStyle != "undefined") {
+                PRO_addStyle(css);
+            } else {
+                var heads = document.getElementsByTagName("head");
+                if (heads.length > 0) {
+                    var node = document.createElement("style");
+                    node.type = "text/css";
+                    node.appendChild(document.createTextNode(css));
+                    heads[0].appendChild(node);
+                    node = null;
+                }
             }
-        }
-    };
+        };
+    }
 
     var onReady = function () {
+        addStyle(".tweet .cannot-retweet{display: inline !important;}");
+
         var retweetDialog = document.getElementById("retweet-tweet-dialog"),
             globalDialog = document.getElementById("global-tweet-dialog");
 
         var getOriginalTweetText = function () {
             var originalTweetText,
-                tweetTextElement,
                 tweetTextElementText,
                 tweetTextElements,
-                tweetTextElementsLength,
-                i;
+                screenName = retweetDialog.querySelector("div.content div.stream-item-header span.username").
+                    textContent.trim();
+
             tweetTextElements = retweetDialog.querySelector("div.content p.js-tweet-text").childNodes;
-            tweetTextElementsLength = tweetTextElements.length;
             originalTweetText = "";
-            for (i = 0; i < tweetTextElementsLength; i += 1) {
-                tweetTextElement = tweetTextElements[i];
+            Array.prototype.forEach.call(tweetTextElements, function (tweetTextElement) {
                 tweetTextElementText = "";
                 if (tweetTextElement.nodeType === TEXT_NODE) {
                     tweetTextElementText = tweetTextElement.textContent;
@@ -113,25 +112,27 @@
                         tweetTextElementText = tweetTextElement.textContent;
                     }
                 }
-                originalTweetText += tweetTextElementText;
-            }
-            return "RT " + retweetDialog.querySelector("div.content div.stream-item-header span.username").
-                textContent.trim() + ": " + originalTweetText.trim().replace(/\s{2,}/g, " ");
+                if (tweetTextElementText) {
+                    originalTweetText += tweetTextElementText;
+                }
+            });
+            return "RT " + screenName + ": " + originalTweetText.trim().replace(/\s{2,}/g, " ");
         };
 
-        var clickElement = function (el) {
+        var invokeClick = function (el) {
             var evt = document.createEvent("Event");
+
             evt.initEvent('click', true, true);
             el.dispatchEvent(evt);
         };
 
         var hideRetweetDialog = function () {
-            clickElement(document.querySelector("#retweet-tweet-dialog button.modal-close"));
+            invokeClick(document.querySelector("#retweet-tweet-dialog button.modal-close"));
         };
 
         var showGlobalTweetDialog = function () {
             globalDialog.querySelector(".draggable").setAttribute("style", retweetDialog.querySelector(".draggable").getAttribute("style"));
-            clickElement(document.querySelector("button#global-new-tweet-button"));
+            invokeClick(document.querySelector("button#global-new-tweet-button"));
             globalDialog.querySelector(".modal-title").innerHTML = retweetDialog.querySelector(".modal-title").textContent;
         };
 
@@ -157,6 +158,7 @@
             var editableDiv = globalDialog.querySelector("div.tweet-box"),
                 range = document.createRange(),
                 selection = window.getSelection();
+
             editableDiv.innerHTML = text;
             editableDiv.focus();
             range.selectNodeContents(editableDiv);
@@ -166,14 +168,15 @@
         };
 
         var btnAction = retweetDialog.querySelector("button.cancel-action");
+
         btnAction.className = btnAction.className.replace(/\bcancel-action\b/g, "rt-action");
         btnAction.innerHTML = "RT";
-        jOn(document, "click", "#retweet-tweet-dialog .original-tweet", function (event) {
-            if (event.target.tagName.toLowerCase() === "div") {
+        jOn(retweetDialog, "click", ".original-tweet", function (event) {
+            if (event.target.tagName === "DIV") {
                 whenOnRetweetDialog();
             }
         });
-        jOn(document, "click", "#retweet-tweet-dialog button.rt-action", whenOnRetweetDialog);
+        jOn(retweetDialog, "click", "button.rt-action", whenOnRetweetDialog);
         jOn(document, "mouseup", ".tweet .cannot-retweet span.retweet", function (event) {
             if (event.button == 2) { // Ignore right-clicks
                 return;
@@ -185,7 +188,6 @@
         });
     };
 
-    addCSS(".tweet .cannot-retweet{display: inline !important;}");
     if (/complete|loaded|interactive/.test(document.readyState)) {
         onReady();
     }
